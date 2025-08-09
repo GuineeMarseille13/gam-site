338f3899ceac199b12987d3d8c60edebe58e16e2
Aboubacar Sidig DIALLO <aboubacar.sidig@gmail.com>
2025-08-09 02:28:28 +0200

    Mise en place du background commun

---
+++ src/components/BackgroundLogo.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Image from "next/image";
+
+export default function BackgroundLogo() {
+  return (
+    <>
+      {/* Logo principal au centre avec effet de parallaxe */}
+      <div className="background-logo background-logo-parallax">
+        <Image
+          src="/images/gam-logo.png"
+          alt="GAM Logo Background"
+          width={1000}
+          height={1000}
+          className="block w-full h-auto"
+          priority={false}
+          quality={75}
+          style={{
+            width: "100%",
+            height: "auto",
+            objectFit: "contain",
+          }}
+        />
+      </div>
+    </>
+  );
+}
